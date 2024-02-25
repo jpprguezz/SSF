9217a20eb3ab30b5a274b15de5470f1ae614706d
José Manuel Peña Rodríguez <josepenrod@gmail.com>
2024-02-25 11:38:18 +0000

    Se hacen cambios

--- Trabajo_Script_Gestion_Procesos/Script_Procesos.sh
+++ Trabajo_Script_Gestion_Procesos/Script_Procesos.sh
@@ -12,4 +12,10 @@
 #   2023-02-25: version 1.0
 #############################################################################
 
-read -p '
+maxCPU=$1 
+maxMem=$2 
+if [[ -z $1 or $2 ]]; then
+    echo 'Error'
+
+read -p "Introduzca el maximo de porcentaje de CPU: " maxCPU
+read -p "Introduzca el maximo de porcentaje de memoria: " maxMem
