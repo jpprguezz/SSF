0ecdaa8ad8b04aeaa3af4a8833512c688de0500a
José Manuel Peña Rodríguez <josepenrod@gmail.com>
2024-02-25 11:28:37 +0000

    Creada ficha técnica del proyecto

---
+++ Trabajo_Script_Gestion_Procesos/Script_Procesos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##############################################################################
+# Nombre: Script_Procesos.sh
+# Autores: José Manuel Peña Rodríguez y Miguel Ángel
+#
+# Objetivo: Gestionar Procesos 
+#
+# Entradas: maxCPU, maxMem
+# Salidas: Resultado de la opción seleccionada
+#
+# Historial:
+#   2023-02-25: version 1.0
+#############################################################################
+
+read -p '
